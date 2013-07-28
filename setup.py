1c1c5b5f15c50b1f47459bb7468867bb37d7eb88
Nathan Wilcox <nejucomo@gmail.com>
2013-07-28 12:40:13 -0700

    Attempt to make setup.py test run flake8, unittests, then doctests (unimplemented); flake8 exits early.

--- setup.py
+++ setup.py
@@ -1,33 +1,66 @@
 #! /usr/bin/env python
 
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+from setuptools.command import test
 
 
-if 'upload' in sys.argv:
-    if '--sign' not in sys.argv and sys.argv[1:] != ['upload', '--help']:
-        raise SystemExit('Refusing to upload unsigned packages.')
+
+def main():
+    if 'upload' in sys.argv:
+        if '--sign' not in sys.argv and sys.argv[1:] != ['upload', '--help']:
+            raise SystemExit('Refusing to upload unsigned packages.')
 
 
-setup(name = 'dryopt',
-      description = "Don't Repeat Yourself option parsing.",
-      url = 'https://github.org/nejucomo/dryopt',
-      license = 'GPLv3',
-      version = '0.1.dev0', # Make sure this is PEP 440 compliant.
-      author = 'Nathan Wilcox',
-      author_email = 'nejucomo@gmail.com',
-      packages = find_packages(),
+    setup(name = 'dryopt',
+          description = "Don't Repeat Yourself option parsing.",
+          url = 'https://github.org/nejucomo/dryopt',
+          license = 'GPLv3',
+          version = '0.1.dev0', # Make sure this is PEP 440 compliant.
+          author = 'Nathan Wilcox',
+          author_email = 'nejucomo@gmail.com',
+          packages = find_packages(),
 
-      # Runtime dependencies:
-      install_requires = [],
+          # Runtime dependencies:
+          install_requires = [],
 
-      # dev-time dependencies:
-      setup_requires = ['flake8 >= 2.0'],
+          # dev-time dependencies:
+          setup_requires = ['flake8 >= 2.0'],
 
-      # Our test command runs flake8, then unittests:
-      command_options = {
-        'aliases': {
-            'test': ('setup.py', 'flake8 test'),
+          cmdclass = {
+            # Rename the standard "test" command to "test_unit":
+            'test_unit': test.test,
+
+            # Add our test_doc command:
+            'test_doc': test_doc,
             },
-        }
-      )
+
+          command_options = {
+            'aliases': {
+                # Our test command runs flake8, then unittests, then doctests:
+                'test': ('setup.py', 'test_flake8 test_unit test_doc'),
+
+                # Rename the "flake8" command to "test_flake8"
+                'test_flake8': ('setup.py', 'flake8'),
+                },
+            }
+          )
+
+
+class test_doc (Command):
+    """A distutils command to run doctests on all modules."""
+
+    description = 'Test all docstring examples.'
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    main()
