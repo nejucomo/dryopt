5c700797ea0640c155875f5ad90c45d81f4b93f7
Nathan Wilcox <nejucomo@gmail.com>
2013-07-28 10:29:00 -0700

    Add a ./setup.py test_pyflakes command.

--- setup.py
+++ setup.py
@@ -1,21 +1,54 @@
 #! /usr/bin/env python
 
 import sys
-from setuptools import setup, find_packages
+import subprocess
+from setuptools import setup, find_packages, Command
 
 
-if 'upload' in sys.argv:
-    if '--sign' not in sys.argv and sys.argv[1:] != ['upload', '--help']:
-        raise SystemExit('Refusing to upload unsigned packages.')
+PKGNAME = 'dryopt'
 
 
-setup(name = 'dryopt',
-      description = "Don't Repeat Yourself option parsing.",
-      url = 'https://github.org/nejucomo/dryopt',
-      license = 'GPLv3',
-      version = '0.1.dev0', # Make sure this is PEP 440 compliant.
-      author = 'Nathan Wilcox',
-      author_email = 'nejucomo@gmail.com',
-      packages = find_packages(),
-      install_requires = [],
-      )
+def main():
+    if 'upload' in sys.argv:
+        if '--sign' not in sys.argv and sys.argv[1:] != ['upload', '--help']:
+            raise SystemExit('Refusing to upload unsigned packages.')
+
+
+    setup(name = PKGNAME,
+          description = "Don't Repeat Yourself option parsing.",
+          url = 'https://github.org/nejucomo/dryopt',
+          license = 'GPLv3',
+          version = '0.1.dev0', # Make sure this is PEP 440 compliant.
+          author = 'Nathan Wilcox',
+          author_email = 'nejucomo@gmail.com',
+          packages = find_packages(),
+          install_requires = [],
+
+          cmdclass = {
+            "test_pyflakes": TestPyflakes,
+            },
+          )
+
+
+class TestPyflakes (Command):
+    """Run pyflakes."""
+
+    description = 'Run pyflakes.'
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        status = subprocess.call(['pyflakes', PKGNAME])
+        if status != 0:
+            raise SystemExit(status)
+
+
+
+if __name__ == '__main__':
+    main()
